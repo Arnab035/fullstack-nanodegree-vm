8f224dc60f785f30ca933bea94714c5c22bf03b3
Arnab035 <arnabjyotikalita35@gmail.com>
2015-11-01 23:24:19 +0530

    Add some more functionality to check for even number of players

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -8,7 +8,10 @@ import psycopg2
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
-    return psycopg2.connect("dbname=tournament")
+    try:
+        return psycopg2.connect("dbname=tournament")
+    except:
+        print("error trying to connect")
 
 
 def deleteMatches():
@@ -51,17 +54,16 @@ def countPlayers():
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
     Args:
       name: the player's full name (need not be unique).
     """
     db = connect()
     c = db.cursor()
 
-    # registering a player for the tournament 
+    # registering a player for the tournament
     c.execute("insert into players(player_name) values (%s)", (name, ))
     db.commit()
     db.close()
@@ -101,21 +103,19 @@ def reportMatch(winner, loser):
     """
     db = connect()
     c = db.cursor()
-    # insertion of winner and the loser for a particular match 
+    # insertion of winner and the loser for a particular match
     c.execute("insert into matches(player_winner,player_loser) values (%s,%s)",
               (winner, loser, ))
     db.commit()
     db.close()
- 
- 
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -124,26 +124,22 @@ def swissPairings():
         name2: the second player's name
     """
     # playerStandings fetches the current player standings based on a view
-    # player_standings 
+    # player_standings
     standings = playerStandings()
 
     # count contains the number of players
     count = len(standings)
-    i = 0
-    
-    # pairs is a list of tuples that will contain the swiss-system pairs 
     pairs = []
-    
-    while (i < count):
-        
-        # the swiss-system pairs are based on selecting adjacent players
-        # from the list of player standings
-        row = standings[i]
-        next_row = standings[i+1]
-        pairs.append((row[0], row[1], next_row[0], next_row[1])) 
+    if (count % 2 == 0):
+        i = 0
+    # pairs is a list of tuples that will contain the swiss-system pairs
+        while (i < count):
+            # the swiss-system pairs are based on selecting adjacent
+            # players from the list of player standings
+            row = standings[i]
+            next_row = standings[i+1]
+            pairs.append((row[0], row[1], next_row[0], next_row[1]))
 
         # iterate over two elements
-        i = i + 2
+            i = i + 2
     return pairs
-
-
